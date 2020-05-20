f966c4d712bf0e1113a586a42109ebac8da22f88
therealdarkflamemaster <2392732614@qq.com>
2020-05-21 01:34:38 +0200

    upload 5/21

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("application");
+        // create a printer object
+        MessagePrinter printer = new MessagePrinter();
+
+    }
+}
---
+++ src/main/java/hello/MessageService.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * print service
+ */
+
+
+public class MessageService {
+    /**
+     * run the print service
+     * @return string type
+     */
+    public String getMessage(){
+        return "Hello, world!";
+    }
+}
